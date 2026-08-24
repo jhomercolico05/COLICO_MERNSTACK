31e2b18dc91c5633d93a15a4389f62257232d179
jhomercolico05 <jhomer.colico@cvsu.edu.ph>
2026-08-24 18:42:58 +0800

    first commit

--- COLICO_MERNSTACK/src/App.jsx
+++ COLICO_MERNSTACK/src/App.jsx
@@ -1,5 +1,5 @@
 import StudentCard from "./components/student-card.jsx";
-
+import SubjectCard from "./components/subject.jsx";
 
 function App(){
   return (
@@ -12,7 +12,20 @@ function App(){
         course = "BSIT"
       />
 
+      <SubjectCard
+        schedCode={202401111}
+        subjectName="DCIT26 App Dev"
+        classSchedule={20001234}
+      />
+
+      <SubjectCard
+        schedCode={202402222}
+        subjectName="DCIT26 App Dev 123"
+        classSchedule={200054321}
+      />
     </div>
+
+    
   );
 }
 
---
+++ COLICO_MERNSTACK/src/components/subject.jsx
@@ -0,0 +1,14 @@
+function subjectCard({schedCode, subjectName, classSchedule}) {
+    return (
+        <div className="bg-orange-200 border border-black-200 rounded-lg p-10 m-20">
+            <p className="text-green-500">Sched Code: {schedCode}</p>
+            <p className="text-yellow-500">Subject Name: {subjectName}</p>
+            <p className="text-green-500">Class Schedule: {classSchedule}</p>
+
+        </div>
+    )
+}
+
+
+
+export default subjectCard;
